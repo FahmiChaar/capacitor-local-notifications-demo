74328829423e48e72a94ef06d85aed513c6da61c
fahmi <fahmichaar@gmail.com>
2022-10-24 11:26:48 +0100

    Initial Commit

---
+++ android/app/src/main/java/com/test/ln/MainActivity.java
@@ -0,0 +1,5 @@
+package com.test.ln;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Channel, LocalNotifications, LocalNotificationSchema } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,33 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  async createChannel(): Promise<Channel> {
+    const channelsList = await LocalNotifications.listChannels();
+    channelsList.channels.forEach(async (c) => await LocalNotifications.deleteChannel(c));
+    const channel: Channel = {
+      id: '' + new Date().getTime(),
+      name: 'Local Notifications Channel',
+      importance: 5,
+      visibility: 1,
+      vibration: true,
+    };
+    await LocalNotifications.createChannel(channel);
+    return channel;
+  }
+
+  async schedule() {
+    const channel = await this.createChannel();
+    const notifications: LocalNotificationSchema[] = [{
+      title: 'Local notifications',
+      body: 'Local Notifications Test app',
+      id: new Date().getTime(),
+      schedule: { at: new Date(), allowWhileIdle: true },
+      channelId: channel.id,
+    }];
+    LocalNotifications.schedule({
+      notifications
+    });
+  }
 
 }
