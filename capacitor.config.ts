3228bdc9685747c097a73e2c1c5f5874b1ee19ab
fahmi <fahmichaar@gmail.com>
2022-10-24 11:03:50 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.test.ln',
+  appName: 'test-local-notifications',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
